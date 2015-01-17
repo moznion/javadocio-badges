6a5b723970a67c4e3cbd4e00e1339a7d7600782b
moznion <moznion@gmail.com>
2015-01-17 18:18:32 +0900

    Revert ":sweat:"
    This reverts commit 3f92bc2cb6f18a081ee264a8d362644b71b1e400.

--- src/test/java/net/moznion/javadocio/badges/controller/RootControllerTest.java
+++ src/test/java/net/moznion/javadocio/badges/controller/RootControllerTest.java
@@ -1,5 +1,45 @@
 package net.moznion.javadocio.badges.controller;
 
+import me.geso.mech2.Mech2;
+import me.geso.mech2.Mech2Result;
+import me.geso.mech2.Mech2WithBase;
+import org.apache.catalina.Globals;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class RootControllerTest {
+    private Mech2WithBase mech;
+    private Tomcat tomcat;
+
+    @Before
+    public void before() throws ServletException, LifecycleException, URISyntaxException {
+        this.tomcat = new Tomcat();
+        tomcat.setPort(0);
+        org.apache.catalina.Context webContext = tomcat.addWebapp("/", new File("src/main/webapp").getAbsolutePath());
+        webContext.getServletContext().setAttribute(Globals.ALT_DD_ATTR, "src/main/webapp/WEB-INF/web.xml");
+        tomcat.start();
+
+        int port = tomcat.getConnector().getLocalPort();
+        String url = "http://127.0.0.1:" + port;
+        this.mech = new Mech2WithBase(Mech2.builder().build(), new URI(url));
+    }
+
+    @After
+    public void after() throws Exception {
+        if (this.tomcat != null) {
+            this.tomcat.stop();
+        }
+    }
 }
